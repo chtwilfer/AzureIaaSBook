2d52a8bd07495df1ffe7c32dee1140d829a84457
Pete Zerger <pete.zerger@gmail.com>
2016-02-23 08:51:32 -0600

    Adding Chap 2

---
+++ Chapter 2/DeployNewVM.ps1
@@ -0,0 +1,38 @@
+﻿$vmname = read-host “Please Enter VM Name”
+import-module AzureRM
+$rg = "SCDEMO"
+$loc = "North Europe"
+
+Select-AzureRmSubscription -SubscriptionName "testsubscription"
+
+$storacct = Get-AzureRmStorageAccount -StorageAccountName “StorageAccount” -ResourceGroupName $rg
+
+$diskname = $vmname + "_OSDisk"
+
+$vhduri = $storacct.PrimaryEndpoints.Blob.OriginalString + "vhds/${diskname}.vhd"
+
+$image = get-azurermvmimage -Location $loc -PublisherName "MicrosoftWindowsServer" -Offer "WindowsServer" -Skus "2012-R2-Datacenter" -Version 4.0.20150916
+
+$vNet = Get-AzureRmVirtualNetwork
+
+$subnet = Get-AzureRmVirtualNetworkSubnetConfig -VirtualNetwork $vNet
+
+$nic = New-AzureRmNetworkInterface -Name "${vmname}_nic1" -Subnet $subnet -Location $loc -ResourceGroupName $rg 
+
+$secpassword = convertto-securestring "LS1setup!" -asplaintext -force
+
+$username = "msadmin"
+
+$creds = New-Object System.Management.Automation.PSCredential($username, $secpassword)
+
+$vmconfig = New-AzureRmVMConfig  -VMName $vmname -VMSize "Standard_A2"
+
+Set-AzureRmVMOperatingSystem -Windows -VM $vmconfig -ProvisionVMAgent -EnableAutoUpdate -Credential $creds -ComputerName $vmname
+
+Set-AzureRmVMSourceImage -VM $vmconfig -PublisherName $image.PublisherName -Offer $image.Offer -Skus $image.Skus -Version $image.Version
+
+Add-AzureRmVMNetworkInterface -VM $vmconfig -Id $nic.Id
+
+Set-AzureRmVMOSDisk -VM $vmconfig -Name $diskname -VhdUri $vhduri -Caching ReadWrite -CreateOption fromImage
+
+New-AzureRMVM -ResourceGroupName $rg -Location $loc -VM $vmconfig 
---
+++ Chapter 2/StartAzureVMs.ps1
@@ -0,0 +1,40 @@
+﻿workflow StartVMs 
+{    
+      param() 
+ 
+       $Connection = "Azure Account Conn" 
+       $Cert = "Azure Cert" 
+       $vmname = "TestVM"
+       $resourcegroupname = "VM Resources"
+ 
+ 
+   
+    $Con = Get-AutomationConnection -Name $Connection 
+    if ($Con -eq $null) 
+    { 
+        Write-Output "$Connection does not exist as an item in the automation service. Please contact your admin to publish one"    
+    } 
+    else 
+    { 
+        $SubID = $Con.SubscriptionID 
+        $mgmtCert = $Con.AutomationCertificateName 
+        
+    }    
+ 
+  
+    $AutoCert = Get-AutomationCertificate -Name $Cert 
+    if ($AutoCert -eq $null) 
+    { 
+        Write-Output "$Cert does not exist as an item in the automation service. Please contact your admin to publish one"    
+    } 
+    else 
+    { 
+        $To = $Cert.Thumbprint 
+    } 
+ 
+         
+         Set-AzureRMSubscription -SubscriptionName "Azure Subscription" -Certificate $Cert -SubscriptionId $SubID 
+         Select-AzureRMSubscription -SubscriptionName "Azure Subscription" 
+         Start-AzureRMVM -ResourceGroupName $resourcegroupname -Name $VMName
+
+} 
